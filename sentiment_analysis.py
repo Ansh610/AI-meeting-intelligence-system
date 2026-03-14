9b01b871000805329e6c367ef442243234de869d
Ansh610 <ansh610kumar@gmail.com>
2026-03-14 23:30:03 +0530

    specify sentiment model

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,15 @@
 from transformers import pipeline
 
-sentiment_model = pipeline("sentiment-analysis")
+sentiment_model = pipeline(
+    "sentiment-analysis",
+    model="distilbert-base-uncased-finetuned-sst-2-english"
+)
 
 def analyze_sentiment(text):
 
     result = sentiment_model(text[:512])
 
     label = result[0]["label"]
-    score = round(result[0]["score"]*100,2)
+    score = round(result[0]["score"] * 100, 2)
 
     return f"{label} ({score}%)"
